29f2daa04e3104d56f32b89a0ccf246e8c067852
Gary Russell <grussell@pivotal.io>
2017-11-06 11:35:32 -0500

    Fix Error Handling

--- demo 2/src/main/java/com/example/demo/ErrorTransformerViaSCS.java
+++ demo 2/src/main/java/com/example/demo/ErrorTransformerViaSCS.java
@@ -1,20 +1,14 @@
 package com.example.demo;
 
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.SendTo;
 
-import static com.example.demo.CustomDlqMessageChannel.CUSTOM_DLQ_TOPIC;
-
-@EnableBinding(Processor.class)
+//@EnableBinding(Processor.class)
 public class ErrorTransformerViaSCS {
     public static final String ERROR_CHANNEL = "errorChannel";
 
-    @StreamListener(ERROR_CHANNEL)
-    @SendTo(CUSTOM_DLQ_TOPIC)
+//    @StreamListener(ERROR_CHANNEL)
+//    @SendTo(CUSTOM_DLQ_TOPIC)
     public ErrorWrapper onError(Message message) {
         return new ErrorWrapper();
     }
--- demo 2/src/main/java/com/example/demo/ErrorTransformerViaSI.java
+++ demo 2/src/main/java/com/example/demo/ErrorTransformerViaSI.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import static com.example.demo.ErrorTransformerViaSCS.ERROR_CHANNEL;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
-import static com.example.demo.ErrorTransformerViaSCS.ERROR_CHANNEL;
-
 @Service
+@EnableBinding(CustomDlqMessageChannel.class)
 public class ErrorTransformerViaSI {
 
     @ServiceActivator(inputChannel=ERROR_CHANNEL, outputChannel="customDlqTopic")
     public ErrorWrapper onError(Message message) {
         return new ErrorWrapper();
     }
+
 }
+
